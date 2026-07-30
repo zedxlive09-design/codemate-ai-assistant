340390e9a91a51b0769f5a2974b18d309a13b84d
Z User <z@container>
2026-07-30 15:22:13 +0000

    Debug: Add minimal test App to isolate blank screen issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.test'  // Using test version for debugging
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
